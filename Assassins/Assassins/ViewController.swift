09556a61228e04c80ac7d21156b8d65dc870660a
Alvin Tsui <awtsui@uci.edu>
2020-02-01 01:07:54 -0800

    New Project
    created new project

---
+++ Assassins/Assassins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assassins
+//
+//  Created by Alvin Tsui on 2/1/20.
+//  Copyright © 2020 HackUCI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
