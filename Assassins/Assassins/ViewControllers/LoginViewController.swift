7dfdd7f6ef2deb0659888085768eb80ca8fd6126
Alvin Tsui <awtsui@uci.edu>
2020-02-01 16:12:23 -0800

    updated UI of create/join game
    updated

---
+++ Assassins/Assassins/ViewControllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Assassins
+//
+//  Created by Joshua Liu on 2/1/20.
+//  Copyright © 2020 HackUCI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
