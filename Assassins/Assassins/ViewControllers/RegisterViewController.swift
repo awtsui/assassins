f69c99c7b174d886ad1f609a53d4b847727f4f59
Joshua Liu <joshua.benjamin.liu@gmail.com>
2020-02-01 17:06:26 -0800

    Register Page Creation

---
+++ Assassins/Assassins/ViewControllers/RegisterViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterViewController.swift
+//  Assassins
+//
+//  Created by Joshua Liu on 2/1/20.
+//  Copyright © 2020 HackUCI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController : UIViewController {
+    
+    override func viewDidLoad() {
+           super.viewDidLoad()
+        
+    }
+
+}
